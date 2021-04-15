f3b8734649b538492e8dee3050e34d85cab423f3
Julia <utrobinajulia@gmail.com>
2021-04-15 17:27:58 +0100

    Added lazy loading to load products module separately

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -9,7 +9,6 @@ import {RouterModule} from '@angular/router';
 import {ProductResolver} from './product-resolver.service';
 import {ProductEditTagsComponent} from './product-edit/product-edit-tags.component';
 import {ProductEditInfoComponent} from './product-edit/product-edit-info.component';
-import {AuthGuard} from '../user/auth.guard';
 import {ProductEditGuard} from './product-edit/product-edit.guard';
 
 @NgModule({
@@ -17,31 +16,25 @@ import {ProductEditGuard} from './product-edit/product-edit.guard';
     SharedModule,
     RouterModule.forChild([
       {
-        path: 'products',
-        canActivate: [AuthGuard],
-        children: [
-          {
-            path: '',
-            component: ProductListComponent,
-          },
-          {
-            path: ':id',
-            component: ProductDetailComponent,
-            resolve: {resolvedData: ProductResolver}
-          },
-          {
-            path: ':id/edit',
-            component: ProductEditComponent,
-            resolve: {resolvedData: ProductResolver},
-            canDeactivate: [ProductEditGuard],
-            children: [
-              {path: '', redirectTo: 'info', pathMatch: 'full'},
-              {path: 'info', component: ProductEditInfoComponent},
-              {path: 'tags', component: ProductEditTagsComponent}
-            ]
-          }
-        ]
+        path: '',
+        component: ProductListComponent,
       },
+      {
+        path: ':id',
+        component: ProductDetailComponent,
+        resolve: {resolvedData: ProductResolver}
+      },
+      {
+        path: ':id/edit',
+        component: ProductEditComponent,
+        resolve: {resolvedData: ProductResolver},
+        canDeactivate: [ProductEditGuard],
+        children: [
+          {path: '', redirectTo: 'info', pathMatch: 'full'},
+          {path: 'info', component: ProductEditInfoComponent},
+          {path: 'tags', component: ProductEditTagsComponent}
+        ]
+      }
     ])
   ],
   declarations: [
